4bf9491de30783ba28064f28033eb41397b93f5f
AndreHuerto <diegohuertopeu.edu.pe>
2021-10-21 22:12:20 -0500

    ejercicio 3

---
+++ bin/app_http3.dart
@@ -0,0 +1,17 @@
+import 'dart:convert' as convert;
+
+import 'package:app_http3/clases/clase3.dart';
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) {
+  final url = Uri.parse('http://localhost:3000/estudiante');
+  http.get(url).then((res) {
+    final respuesta = welcomeFromJson(res.body);
+    print('${respuesta}');
+    for (int i = 0; i < respuesta.length; i++) {
+      print(respuesta[i].apellidos);
+    }
+  });
+
+
+}
